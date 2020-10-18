1c0f75d678eb2d3e633e761d0d49d76628883fe4
shristibaranwal4227 <56750177+shristibaranwal4227@users.noreply.github.com>
2020-10-18 14:05:43 +0530

    Add files via upload

---
+++ appointy_meeting_scheduling_API/config/package controller.go
@@ -0,0 +1,40 @@
+package controller
+
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+)
+
+var Login = func(w http.ResponseWriter, r *http.Request) {
+	user := &models.User{}
+	err := json.NewDecoder(r.Body).Decode(user)
+	if err != nil {
+		u.Respond(w, u.Message(false, "Invalid request"))
+		return
+	}
+
+	user.Password = ""
+	tk := &models.Token{UserId: user.Email}
+	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), tk)
+	tokenString, _ := token.SignedString([]byte(os.Getenv("token_password")))
+
+	resp := u.Message(true, "Successful")
+	resp["data"] = user
+	resp["token"] = tokenString
+	u.Respond(w, resp)
+}
+
+var GetUser = func(w http.ResponseWriter, r *http.Request) {
+	user1 := &models.User{}
+	user2 := &models.User{}
+	user1.Email = "cao.trung.thu@mail.com"
+	user2.Email = "cao.trung.thu@hot.com"
+
+	var users [2]*models.User
+	users[0] = user1
+	users[1] = user2
+	resp := u.Message(true, "Successful")
+	resp["data"] = users
+	u.Respond(w, resp)
+}
---
+++ appointy_meeting_scheduling_API/config/package routes.go
@@ -0,0 +1,91 @@
+package routes
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gorilla/mux"
+	"github.com/shris1611/jwt-go"
+)
+
+var SetupServer = func(appPort string) {
+	var router = mux.NewRouter()
+
+	router.HandleFunc("/api/login", controller.Login).Methods("POST")
+	router.HandleFunc("/api/getUser", controller.GetUser).Methods("GET")
+
+	router.Use(JwtAuthentication)
+
+	err := http.ListenAndServe(":"+appPort, router)
+	if err != nil {
+		fmt.Print(err)
+	}
+}
+
+var JwtAuthentication = func(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		notAuth := []string{"/api/login"}
+		requestPath := r.URL.Path //current request path
+
+		//check if request does not need authentication, serve the request if it doesn't need it
+		for _, value := range notAuth {
+
+			if value == requestPath {
+				next.ServeHTTP(w, r)
+				return
+			}
+		}
+
+		response := make(map[string]interface{})
+		tokenHeader := r.Header.Get("Authorization") //Grab the token from the header
+
+		if tokenHeader == "" { //Token is missing, returns with error code 403 Unauthorized
+			response = u.Message(false, "Missing auth token")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			u.Respond(w, response)
+			return
+		}
+
+		splitted := strings.Split(tokenHeader, " ") //The token normally comes in format `Bearer {token-body}`, we check if the retrieved token matched this requirement
+		if len(splitted) != 2 {
+			response = u.Message(false, "Invalid/Malformed auth token")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			u.Respond(w, response)
+			return
+		}
+
+		tokenPart := splitted[1] //Grab the token part, what we are truly interested in
+		tk := &models.Token{}
+
+		token, err := jwt.ParseWithClaims(tokenPart, tk, func(token *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("token_password")), nil
+		})
+
+		if err != nil { //Malformed token, returns with http code 403 as usual
+			response = u.Message(false, "Malformed authentication token")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			u.Respond(w, response)
+			return
+		}
+
+		if !token.Valid { //Token is invalid, maybe not signed on this server
+			response = u.Message(false, "Token is not valid.")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			u.Respond(w, response)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "user", tk.UserId)
+		r = r.WithContext(ctx)
+		next.ServeHTTP(w, r) //proceed in the middleware chain!
+	})
+}
