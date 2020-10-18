85abff15343749d768bfc7e0c4e64d102647b421
shristibaranwal4227 <56750177+shristibaranwal4227@users.noreply.github.com>
2020-10-19 00:05:22 +0530

    Update app.go

--- appointy_meeting_scheduling_API/app/app.go
+++ appointy_meeting_scheduling_API/app/app.go
@@ -1,4 +1,4 @@
-package application
+package app
 
 import (
 	"fmt"
@@ -10,12 +10,12 @@ import (
 )
 
 // Application has router and db instances
-type Application struct {
+type app struct {
 	Router *mux.Router
 	DB     *gorm.DB
 }
 
-// Initialize initializes the application with predefined configuration
+// Initialize initializes the app with predefined configuration
 func (a *App) Initialize(config *config.Config) {
 	dbURI := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s&parseTime=True",
 		config.DB.Username,
@@ -57,33 +57,33 @@ func (a *App) setRouters() {
 }
 
 // Get wraps the router for GET method
-func (a *Application) Get(path string, f func(w http.ResponseWriter, r *http.Request)) {
+func (a *app) Get(path string, f func(w http.ResponseWriter, r *http.Request)) {
 	a.Router.HandleFunc(path, f).Methods("GET")
 }
 
 // Post wraps the router for POST method
-func (a *Application) Post(path string, f func(w http.ResponseWriter, r *http.Request)) {
+func (a *app) Post(path string, f func(w http.ResponseWriter, r *http.Request)) {
 	a.Router.HandleFunc(path, f).Methods("POST")
 }
 
 // Put wraps the router for PUT method
-func (a *Application) Put(path string, f func(w http.ResponseWriter, r *http.Request)) {
+func (a *app) Put(path string, f func(w http.ResponseWriter, r *http.Request)) {
 	a.Router.HandleFunc(path, f).Methods("PUT")
 }
 
 // Delete wraps the router for DELETE method
-func (a *Application) Delete(path string, f func(w http.ResponseWriter, r *http.Request)) {
+func (a *app) Delete(path string, f func(w http.ResponseWriter, r *http.Request)) {
 	a.Router.HandleFunc(path, f).Methods("DELETE")
 }
 
 // Run the app on it's router
-func (a *Application) Run(host string) {
+func (a *app) Run(host string) {
 	log.Fatal(http.ListenAndServe(host, a.Router))
 }
 
 type RequestHandlerFunction func(db *gorm.DB, w http.ResponseWriter, r *http.Request)
 
-func (a *Application) handleRequest(handler RequestHandlerFunction) http.HandlerFunc {
+func (a *app) handleRequest(handler RequestHandlerFunction) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		handler(a.DB, w, r)
 	}
